800def79f6953a6bfd4b5b3ee85742fbcbb38d80
Allen McCabe <amccabe@csatf.org>
2017-03-09 11:50:41 -0800

    Update duration reporting to report in max increments of hours

--- src/CommandLine/ReportCommand.php
+++ src/CommandLine/ReportCommand.php
@@ -127,9 +127,11 @@ class ReportCommand extends Command {
             }
 
             if (IS_CLI) {
+                $max_metric = 'h';
+
                 if (! empty($issues)) {
                     Output::set_line_length(static::$output_line_length);
-                    $TotalDuration = new Duration();
+                    $TotalDuration = new Duration($max_metric);
                     $hline = '+' . str_repeat('-', static::$output_line_length - 2) . '+';
 
                     // Print date (or date range)
@@ -144,18 +146,22 @@ class ReportCommand extends Command {
                     Output::line('+' . str_repeat('=', static::$output_line_length - 2) . '+');
 
                     foreach ($issues as $_issue => $data) {
-                        $Duration = new Duration();
+                        $Duration = new Duration($max_metric);
                         $pad_length = static::$output_line_length - 4;
 
+                        // Print Issue
                         if ($_issue !== 'NA') {
-                            Output::line("\033[1m".$_issue."\033[0m", $border);
+                            Output::line("\033[1m".$_issue."\033[0m".' |', $border);
+                            Output::line('+'.str_repeat('-', strlen($_issue) + 2).'+'.str_repeat(' ', ($pad_length - (strlen($_issue) + 1))).'|');
                         }
 
+                        // Print Descriptions
                         foreach ($data['descriptions'] as $issue_task_index => $description) {
                             $_datetime_display = '';
 
                             if (isset($data['dates'][$issue_task_index])) {
                                 $date = $data['dates'][$issue_task_index]->format('D');
+                                // empty line between days/dates (visual grouping of tasks on a given day)
                                 if ($date !== $last_date && ! is_null($last_date)) {
                                     Output::line('', '|');
                                 }
@@ -172,6 +178,7 @@ class ReportCommand extends Command {
                             }
                         }
 
+                        // Print Duration
                         foreach ($data['durations'] as $_duration) {
                             $Duration->add($_duration);
                             $TotalDuration->add($_duration);
@@ -179,6 +186,11 @@ class ReportCommand extends Command {
 
                         Output::line(str_pad($Duration->asString(), $pad_length, ' ', STR_PAD_LEFT), $border);
                         Output::line($hline);
+
+                        if ($_issue !== $last_issue) {
+                            $last_issue = $_issue;
+                            $last_date = null;
+                        }
                     }
 
                     if (count($issues) > 1) {
--- src/Duration.php
+++ src/Duration.php
@@ -14,36 +14,138 @@ class Duration
 {
     private $Base;
 
-    public function __construct()
+    private $DiffInterval;
+
+    private $maximum_interval;
+
+    public function __construct($maximum_interval = null)
     {
         $this->Base = Carbon::now();
+        $this->setMaxInterval($maximum_interval);
+    }
+
+    public function setMaxInterval($maximum_interval = null) {
+        if (! is_null($maximum_interval)) {
+            switch ($maximum_interval) {
+                case 'd':
+                case 'days':
+                    $this->maximum_interval = 'd';
+                    break;
+                case 'h':
+                case 'hrs':
+                case 'hour':
+                case 'hours':
+                    $this->maximum_interval = 'h';
+                break;
+                case 'i':
+                case 'min':
+                case 'mins':
+                case 'minutes':
+                    $this->maximum_interval = 'i';
+                break;
+            }
+        }
     }
 
     public function add(\DateInterval $interval) {
         $this->Base->add($interval);
+        return $this/*->recalculate()*/;
     }
 
-    public function calc() {
-        return Carbon::now()->diff($this->Base);
+    public function diff() {
+        if (! isset($this->DiffInterval)) {
+            $this->DiffInterval = Carbon::now()->diff($this->Base);
+        }
+        return $this->DiffInterval;
+    }
+
+    public function daysString() {
+        if ($DiffInterval = $this->diff()) {
+            if ($DiffInterval->d) {
+                return $DiffInterval->d.($DiffInterval->d == 1 ? ' day' : ' days');
+            }
+        }
+    }
+
+    public function hoursString() {
+        if ($DiffInterval = $this->diff()) {
+            if ($DiffInterval->h) {
+                return $DiffInterval->h.($DiffInterval->h == 1 ? ' hour' : ' hours');
+            }
+        }
+    }
+
+    public function minutesString() {
+        if ($DiffInterval = $this->diff()) {
+            if ($DiffInterval->i) {
+                return $DiffInterval->i.($DiffInterval->i == 1 ? ' min' : ' mins');
+            }
+        }
+    }
+
+    public function intervals_exceed_max() {
+        $exceeds = false;
+        switch ($this->maximum_interval) {
+            case 'i':
+                if ($this->diff()->h > 0) {
+                    $exceeds = true;
+                }
+            case 'h':
+                if ($this->diff()->d > 0) {
+                    $exceeds = true;
+                }
+            case 'd':
+                if ($this->diff()->m > 0) {
+                    $exceeds = true;
+                }
+                break;
+        }
+
+        return $exceeds;
+    }
+
+    public function redistribute() {
+        while ($this->intervals_exceed_max()) {
+            if ($this->maximum_interval == 'd' || $this->maximum_interval == 'h' || $this->maximum_interval == 'i') {
+                if ($this->diff()->m > 0) {
+                    $this->diff()->d += $this->diff()->m * 30;
+                    $this->diff()->m = 0;
+                }
+            }
+            if ($this->maximum_interval == 'h' || $this->maximum_interval == 'i') {
+                if ($this->diff()->d > 0) {
+                    $this->diff()->h += $this->diff()->d * 24;
+                    $this->diff()->d = 0;
+                }
+            }
+            if ($this->maximum_interval == 'i') {
+                if ($this->diff()->h > 0) {
+                    $this->diff()->i += $this->diff()->h * 60;
+                    $this->diff()->h = 0;
+                }
+            }
+        }
+
     }
 
     public function asString() {
-        $duration = '';
-        $DiffInterval = $this->calc();
+        $duration = [];
 
-        if ($DiffInterval->d) {
-            $duration .= $DiffInterval->d.($DiffInterval->d == 1 ? ' day' : ' days');
-        }
-        if ($DiffInterval->h) {
-            if (mb_strlen($duration)) $duration .= ', ';
-            $duration .= $DiffInterval->h.($DiffInterval->h == 1 ? ' hour' : ' hours');
-        }
-        if ($DiffInterval->i) {
-            if (mb_strlen($duration)) $duration .= ', ';
-            $duration .= $DiffInterval->i.($DiffInterval->i == 1 ? ' min' : ' mins');
+        if (isset($this->maximum_interval)) {
+            $this->redistribute();
         }
 
-        return $duration;
+        if ($days = $this->daysString()) {
+            $duration[] = $days;
+        }
+        if ($hours = $this->hoursString()) {
+            $duration[] = $hours;
+        }
+        if ($minutes = $this->minutesString()) {
+            $duration[] = $minutes;
+        }
+
+        return implode(', ', $duration);
     }
 
     public function __call($name, $arguments) {
